843bde241f6179f518f9663fb5553d369526b15a
pragy-dl <aakeshaaka@gmail.com>
2024-02-06 01:43:14 +0530

    Update restart.py
    Co-Authored-By: Avyukt X <137268306+AvyuktX@users.noreply.github.com>

--- PrikshaXMusic/plugins/sudo/restart.py
+++ PrikshaXMusic/plugins/sudo/restart.py
@@ -18,7 +18,7 @@ from PrikshaXMusic.utils.database import (
     remove_active_video_chat,
 )
 from PrikshaXMusic.utils.decorators.language import language
-from PrikshaXMusic.utils.pastebin import PrikshaBin
+from PrikshaXMusic.utils.pastebin import PrikshaXBin
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -67,7 +67,7 @@ async def update_(client, message, _):
     _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
     _final_updates_ = _update_response_ + updates
     if len(_final_updates_) > 4096:
-        url = await PrikshaBin(updates)
+        url = await PrikshaXBin(updates)
         nrs = await response.edit(
             f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
         )
